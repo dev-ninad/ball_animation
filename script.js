9e92207822e7fa935f80e270fbaa63f83e085a58
dev-ninad <ninadx97@gmail.com>
2023-10-25 14:54:21 +0530

    increase speed

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ const ball = {
   x: Math.random() * canvas.width, // Start at a random x position
   y: Math.random() * canvas.height, // Start at a random y position
   radius: 15,
-  dx: 7, //initial velocity
-  dy: 7,
+  dx: 5, //initial velocity
+  dy: 5,
   color: getRandomColor(),
 };
 
 let animationFrameId;
 let isPaused = false;
+let originalSpeedx = ball.dx; //original speed
+let originalSpeedy = ball.dy;
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
-  for (let i = 0; i < 6; i++) {   //Get the random code for color "#f0f0f0"
+  for (let i = 0; i < 6; i++) {   // Get the random code for color "#f0f0f0"
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
@@ -38,7 +40,7 @@ function updateBallPosition() {
     ball.dx = -ball.dx; //reverse its velocity
     ball.color = getRandomColor();
   }
-  //// Bounce the ball off the left edge by reversing its velocity
+  // Bounce the ball off the left edge by reversing its velocity
   if (ball.y + ball.radius + ball.dy > canvas.height || ball.y - ball.radius + ball.dy < 0) {
     ball.dy = -ball.dy;//reverse its velocity
     ball.color = getRandomColor();
@@ -67,17 +69,20 @@ function startAnimation() {
 
 canvas.addEventListener("click", () => {
   isPaused = !isPaused;
-  document.getElementById('note').style.display = 'none';  //hide the note
+  document.getElementById('note').style.display = 'none';  //Hide the note
+ // Increase the speed on each click
+   ball.dx += 5; 
+   ball.dy += 5;
 });
 
-document.getElementById("resetButton").addEventListener("click", () => { //using event listener to update the animation 
-  document.getElementById('note').style.display = 'block';   //show the note
+document.getElementById("resetButton").addEventListener("click", () => { 
+  //Using event listener to update the animation 
+  document.getElementById('note').style.display = 'block';   //Show the note to user
   cancelAnimationFrame(animationFrameId);
   isPaused = true;
   clearCanvas();
-  ball.x = Math.random() * canvas.width;
-  ball.y = Math.random() * canvas.height;
-  ball.color = getRandomColor();
+  ball.dx = originalSpeedx;  //reset original speed
+  ball.dy = originalSpeedy;
   startAnimation(); // Restart the animation
 });
 
